6514189fc36311fffe86d710852d40756dc6a39f
matheusfilizzola <matheusfilizzola98@gmail.com>
2023-04-18 15:45:01 -0300

    shop npc and ui shop created

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Managers;
+
+public class PlayerController : MonoBehaviour
+{
+    public InputManager myInputs;
+
+    private Rigidbody2D rig;
+    private Animator anim;
+    public Transform body;
+
+    private Vector2 movement;
+
+    public float speedPlayer;
+
+    [SerializeField] private bool isRight;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+    void Update()
+    {
+        Inputs();
+        //SetAnimations();
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void Inputs()
+    {
+        movement = myInputs.input.Player.Move.ReadValue<Vector2>().normalized;
+
+        if (movement.x > 0)
+        {
+            isRight = true;
+        }
+        else if (movement.x < 0)
+        {
+            isRight = false;
+        }
+    }
+
+    private void MovePlayer()
+    {
+        rig.velocity = new Vector2(movement.x * speedPlayer, movement.y * speedPlayer);
+    }
+
+    void Flip()
+    {
+        if (isRight)
+        {
+            body.localScale = new Vector3(1.00846f, body.localScale.y, body.localScale.z);
+        }
+        else
+        {
+            body.localScale = new Vector3(-1.00846f, body.localScale.y, body.localScale.z);
+        }
+
+
+    }
+
+    //private void SetAnimations()
+    //{
+    //    anim.SetFloat("walkSpeed", movement.magnitude);
+    //}
+}
