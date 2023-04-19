141fa3fa1ac667d3d6727e1e8ab98163a84c4b14
matheusfilizzola <matheusfilizzola98@gmail.com>
2023-04-19 15:17:28 -0300

    about screen invetory player created

--- Assets/Scripts/Player/InventoryPlayerManager.cs
+++ Assets/Scripts/Player/InventoryPlayerManager.cs
@@ -1,34 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryPlayerManager : MonoBehaviour
 {
     [SerializeField] private InventorySO inventoryPlayer;
 
     public EquipsPlayer equipsPlayer;
+    public EquipsIlustrationPlayer equipsIlustrationPlayer;
 
     public void UseItem()
     {
         switch (ShopManager.Instance.itemSelected.typeEquips)
         {
             case Equips.Hair:
-                GetEquipsParts(equipsPlayer.hair);
+                GetEquipsParts(equipsPlayer.hair, equipsIlustrationPlayer.hair);
                 break;
             case Equips.Shirt:
-                GetEquipsParts(equipsPlayer.Shirt);
+                GetEquipsParts(equipsPlayer.Shirt, equipsIlustrationPlayer.Shirt);
                 break;
             case Equips.Shoe:
-                GetEquipsParts(equipsPlayer.shoes);
+                GetEquipsParts(equipsPlayer.shoes, equipsIlustrationPlayer.shoes);
                 break;
         }
     }
 
-    private void GetEquipsParts(SpriteRenderer[] spriteReference)
+    private void GetEquipsParts(SpriteRenderer[] spriteReference, Image[] spriteImageReference)
     {
         for (int i = 0; i < spriteReference.Length; i++)
         {
             spriteReference[i].sprite = ShopManager.Instance.itemSelected.sprites[i];
+            spriteImageReference[i].sprite = ShopManager.Instance.itemSelected.sprites[i];
         }
     }
 
@@ -38,6 +41,14 @@ public class InventoryPlayerManager : MonoBehaviour
         public SpriteRenderer[] hair;
         public SpriteRenderer[] Shirt;
         public SpriteRenderer[] shoes;
+    }
+
+    [System.Serializable]
+    public class EquipsIlustrationPlayer
+    {
+        public Image[] hair;
+        public Image[] Shirt;
+        public Image[] shoes;
     }
 }
 
